0e57dd7ed876a9f4841ffa4e9e6fdd4131d67435
swiftsolves-msft <30509195+swiftsolves-msft@users.noreply.github.com>
2023-06-06 11:53:13 -0400

    Update site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,7 +1,7 @@
 ﻿$(document).ready(function () {
     $('.lazy').Lazy({
         scrollDirection: 'vertical',
-        effect: 'fadeIn',
+        effect: 'fadeOut',
         visibleOnly: true,
         onError: function (element) {
             console.log('error loading ' + element.data('src'));
